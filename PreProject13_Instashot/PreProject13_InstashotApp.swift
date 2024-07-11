0d36825c8207145681c85b204fc35dc8be6d9396
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-07-11 05:48:57 -0300

    Initial Commit

---
+++ PreProject13_Instashot/PreProject13_InstashotApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreProject13_InstashotApp.swift
+//  PreProject13_Instashot
+//
+//  Created by Bruno Oliveira on 11/07/24.
+//
+
+import SwiftUI
+
+@main
+struct PreProject13_InstashotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
